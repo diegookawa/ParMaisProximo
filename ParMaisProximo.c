2a6cb2ede4863ae3c6c63a13a70bd944d052798b
diegookawa <diego.arenas1505@gmail.com>
2021-10-21 16:09:59 -0300

    LerArquivo

--- ParMaisProximo.c
+++ ParMaisProximo.c
@@ -1,8 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct ponto {
+    
+    float x;
+    float y;
+
+} Ponto;
+
+Ponto* lerArquivo(char nomeArquivo[], int *tam);
+void imprimirVetor(Ponto pontos[], int tam);
+
 int main(){
 
+    Ponto *pontos;
+    int tam;
+
+    pontos = lerArquivo("teste.txt", &tam);
+    imprimirVetor(pontos, tam);
+    
     return 0;
+
+}
+
+Ponto* lerArquivo(char nomeArquivo[], int *tam){
+
+    FILE *arquivo;
+    Ponto *pontos;
+    arquivo = fopen(nomeArquivo, "r");
+    int tamanhoArquivo;
+
+    if(arquivo == NULL){
+
+        printf("ERRO AO ABRIR O ARQUIVO.");
+        getchar();
+        exit(1);
+
+    }
+
+    fscanf(arquivo, "%d", tam);
+
+    pontos = (Ponto*) malloc ((*tam) * sizeof (Ponto));
+
+    for(int i = 0; i < (*tam); i++)
+        fscanf(arquivo, "%f %f", &pontos[i].x, &pontos[i].y);
+        
+    fclose(arquivo);
+
+    return pontos;
+
+}
+
+void imprimirVetor(Ponto pontos[], int tam){
+
+    for(int i = 0; i < tam; i++)
+        printf("%f %f\n", pontos[i].x, pontos[i].y);
 
 }
